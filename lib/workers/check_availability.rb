cd7f7d432320876e1594130882c3867f3e2d5274
woarewe <rostislav.zhuravsky@gmail.com>
2021-09-16 07:20:49 +0300

    Try full integration

--- lib/workers/check_availability.rb
+++ lib/workers/check_availability.rb
@@ -5,7 +5,19 @@ module Workers
     include Sidekiq::Worker
 
     def perform(*)
-      Core::Tickets.available?(from: "Orša Centraĺnaja", to: "Minsk Pasažyrski", date: "2021-09-19", train: "865Б")
+      result = Core::Tickets.available?(from: "Orša Centraĺnaja", to: "Minsk Pasažyrski", date: "2021-09-19", train: "867Б")
+      return unless result
+
+      body = {
+        chat_id: 385181870,
+        text: "There are available tickets!!!"
+      }
+
+      Faraday.post(
+        "https://api.telegram.org/bot#{ENV.fetch("BOT_API_TOKEN")}/sendMessage",
+        body.to_json,
+        { "Content-Type" => "application/json" }
+      )
     end
   end
 end
